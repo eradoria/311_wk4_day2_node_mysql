dfc17045d86f914382f65266f76a4a04f6246ddd
eradoria <eradoria@gmail.com>
2022-06-23 20:47:00 -0500

    first

--- controllers/users.js
+++ controllers/users.js
@@ -1,67 +1,70 @@
-const mysql = require('mysql')
-const pool = require('../sql/connection')
-const { handleSQLError } = require('../sql/error')
+const mysql = require("mysql");
+const pool = require("../sql/connection");
+const { handleSQLError } = require("../sql/error");
 
 const getAllUsers = (req, res) => {
   // SELECT ALL USERS
   pool.query("SELECT * FROM users", (err, rows) => {
-    if (err) return handleSQLError(res, err)
+    if (err) return handleSQLError(res, err);
     return res.json(rows);
-  })
-}
+  });
+};
 
 const getUserById = (req, res) => {
   // SELECT USERS WHERE ID = <REQ PARAMS ID>
-  let sql = "QUERY GOES HERE"
+  let id = req.params.id;
+  let sql = "SELECT * FROM ?? WHERE ?? = ?";
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = mysql.format(sql, ["users", "id", id]);
 
   pool.query(sql, (err, rows) => {
-    if (err) return handleSQLError(res, err)
+    if (err) return handleSQLError(res, err);
     return res.json(rows);
-  })
-}
+  });
+};
 
 const createUser = (req, res) => {
-  // INSERT INTO USERS FIRST AND LAST NAME 
-  let sql = "QUERY GOES HERE"
+  // INSERT INTO USERS FIRST AND LAST NAME
+  let sql = `INSERT INTO ?? (first_name, last_name) VALUES (null, "${req.body.first_name}","${req.body.last_name})`;
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = mysql.format(sql, ["users"]);
 
   pool.query(sql, (err, results) => {
-    if (err) return handleSQLError(res, err)
+    if (err) return handleSQLError(res, err);
     return res.json({ newId: results.insertId });
-  })
-}
+  });
+};
 
 const updateUserById = (req, res) => {
   // UPDATE USERS AND SET FIRST AND LAST NAME WHERE ID = <REQ PARAMS ID>
-  let sql = ""
+  const { id } = req.params;
+  const { body } = req;
+  let sql = "UPDATE ?? SET ? WHERE ?? = ?";
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = mysql.format(sql, ["user", body, "id", id]);
 
   pool.query(sql, (err, results) => {
-    if (err) return handleSQLError(res, err)
+    if (err) return handleSQLError(res, err);
     return res.status(204).json();
-  })
-}
+  });
+};
 
 const deleteUserByFirstName = (req, res) => {
   // DELETE FROM USERS WHERE FIRST NAME = <REQ PARAMS FIRST_NAME>
-  let sql = ""
+  let sql = "";
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = mysql.format(sql, []);
 
   pool.query(sql, (err, results) => {
-    if (err) return handleSQLError(res, err)
+    if (err) return handleSQLError(res, err);
     return res.json({ message: `Deleted ${results.affectedRows} user(s)` });
-  })
-}
+  });
+};
 
 module.exports = {
   getAllUsers,
   getUserById,
   createUser,
   updateUserById,
-  deleteUserByFirstName
-}
+  deleteUserByFirstName,
+};
