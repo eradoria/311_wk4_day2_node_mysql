1897ffaee1722931b8f62adb4c00984e3216a905
eradoria <eradoria@gmail.com>
2022-06-24 20:43:48 -0500

    secound

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const usersRouter = require('./routers/users');
+// const bodyParser = require("body-parser");
+const usersRouter = require("./routers/users");
 
 const app = express();
 const port = process.env.PORT || 4001;
 
-app.use(bodyParser.json())
-app.use('/users', usersRouter)
+app.use(express.json());
+app.use("/users", usersRouter);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to our server!')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to our server!");
+});
 
 app.listen(port, () => {
- console.log(`Web server is listening on port ${port}!`);
+  console.log(`Web server is listening on port ${port}!`);
 });
--- sql/connection.js
+++ sql/connection.js
@@ -1,24 +1,28 @@
-const mysql = require('mysql')
+const { text } = require("body-parser");
+const mysql = require("mysql");
+
+// const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } = process.env;
 
 class Connection {
   constructor() {
     if (!this.pool) {
-      console.log('creating connection...')
+      console.log("creating connection...");
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'admin'
-      })
+        host: database.cilq1tqutxtq.us-east-1.rds.amazonaws.com,
+        user: admin,
+        password: Password2,
+        database: test,
+        port: 3306,
+      });
 
-      return this.pool
+      return this.pool;
     }
 
-    return this.pool
+    return this.pool;
   }
 }
 
-const instance = new Connection()
+const instance = new Connection();
 
 module.exports = instance;
